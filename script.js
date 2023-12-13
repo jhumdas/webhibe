9a44ae5d9301744d7a60075775e962242a85d03b
jhumdas <dasjhuma0099@gmail.com>
2023-12-13 16:54:52 +0530

    up

---
+++ script.js
@@ -0,0 +1,11 @@
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 100){  
+       $('header').addClass("sticky");
+     }
+     else{
+       $('header').removeClass("sticky");
+     }
+   });
+
+
+
